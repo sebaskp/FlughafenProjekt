cb869c2ec9915a562204f74f18c1105510faca86
benja <benja@Bens_Book.Red5.com>
2025-08-27 14:23:38 +0200

    .

---
+++ Flughafen/src/spe/mch/Flugbegleiter.java
@@ -0,0 +1,15 @@
+package spe.mch;
+
+public class Flugbegleiter extends Person {
+
+	public Flugbegleiter(String vorname, String nachname) {
+		super(vorname, nachname);
+	}
+
+	public Flugbegleiter() {
+		super();
+	}
+
+	
+	
+}
