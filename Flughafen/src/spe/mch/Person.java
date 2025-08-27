1e27b3bd8097fcc7994b61d32ecf617cb0a86e89
benja <benja@Bens_Book.Red5.com>
2025-08-27 12:42:58 +0200

    Person klasee

--- Flughafen/src/spe/mch/Person.java
+++ Flughafen/src/spe/mch/Person.java
@@ -1,7 +1,38 @@
 package spe.mch;
 
-public class Person {
+import java.io.Serializable;
 
+public abstract class Person implements Serializable{
+	private String vorname;
+	private String nachname;
+	
+	public Person(String vorname, String nachname) {
+		super();
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
 
+	public Person() {}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [vorname=" + vorname + ", nachname=" + nachname + "]";
+	}
 
 }
