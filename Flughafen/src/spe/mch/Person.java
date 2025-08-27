f5c51374244d1504ec2f90077942cd98c87cc87e
sebaskp <sebastian.kaehler-pardo@stud.akad.de>
2025-08-27 12:53:57 +0200

    Person Klasse fertig

--- Flughafen/src/spe/mch/Person.java
+++ Flughafen/src/spe/mch/Person.java
@@ -2,6 +2,34 @@ package spe.mch;
 
 public class Person {
 
-
+	private String vorname;
+	private String nachname;
+	
+	public Person(String vorname, String nachname) {
+		super();
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
+	public Person() {}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	@Override
+	public String toString() {
+		return "Person [vorname=" + vorname + ", nachname=" + nachname + "]";
+	}
+	
+	
+	
+	
 
 }
