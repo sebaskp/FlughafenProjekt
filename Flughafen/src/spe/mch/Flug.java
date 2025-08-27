e7e80ed9fd660d2324f732482e0a17dff1519085
benja <benja@Bens_Book.Red5.com>
2025-08-27 14:27:38 +0200

    .

---
+++ Flughafen/src/spe/mch/Flug.java
@@ -0,0 +1,101 @@
+package spe.mch;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Flug {
+
+	private LocalDateTime startzeit;
+	private LocalDateTime landezeit;
+	private String startflughafen;
+	private String zielflughafen;
+	private Flugzeug flugzeug;
+	private ArrayList<Pilot> piloten;
+	private ArrayList<Flugbegleiter> flugbegleiter;
+	private ArrayList<Fluggast> fluggaeste;
+	
+	public ArrayList<Fluggast> getFluggaeste() {
+		return fluggaeste;
+	}
+	public void setFluggaeste(ArrayList<Fluggast> fluggaeste) {
+		this.fluggaeste = fluggaeste;
+	}
+	public Flug(LocalDateTime startzeit, LocalDateTime landezeit, String startflughafen, String zielflughafen) {
+		super();
+		this.startzeit = startzeit;
+		this.landezeit = landezeit;
+		this.startflughafen = startflughafen;
+		this.zielflughafen = zielflughafen;
+	}
+	public Flug() {}
+	
+	public LocalDateTime getStartzeit() {
+		return startzeit;
+	}
+	public void setStartzeit(LocalDateTime startzeit) {
+		this.startzeit = startzeit;
+	}
+	public LocalDateTime getLandezeit() {
+		return landezeit;
+	}
+	public void setLandezeit(LocalDateTime landezeit) {
+		this.landezeit = landezeit;
+	}
+	public String getStartflughafen() {
+		return startflughafen;
+	}
+	public void setStartflughafen(String startflughafen) {
+		this.startflughafen = startflughafen;
+	}
+	public String getZielflughafen() {
+		return zielflughafen;
+	}
+	public void setZielflughafen(String zielflughafen) {
+		this.zielflughafen = zielflughafen;
+	}
+	public Flugzeug getFlugzeug() {
+		return flugzeug;
+	}
+	public void setFlugzeug(Flugzeug flugzeug) {
+		this.flugzeug = flugzeug;
+	}
+	public ArrayList<Pilot> getPiloten() {
+		return piloten;
+	}
+	public void setPiloten(ArrayList<Pilot> piloten) {
+		this.piloten = piloten;
+	}
+	public ArrayList<Flugbegleiter> getFlugbegleiter() {
+		return flugbegleiter;
+	}
+	public void setFlugbegleiter(ArrayList<Flugbegleiter> flugbegleiter) {
+		this.flugbegleiter = flugbegleiter;
+	}
+	
+	public boolean addPilot(Pilot pilot) {
+		if(piloten.size()<2) {
+			piloten.add(pilot);
+			return true;
+		} else {
+			System.out.println("Pilot konnte nicht hinzugefuegt werden, da bereits 2 vorhanden sind");
+			return false;
+			
+		}
+	}
+	
+	public boolean deletePilot(Pilot pilot) {
+		if (piloten.size()>0) {
+			piloten.remove(pilot);
+			return true;
+		} else {
+			System.out.println("Vorgang nicht moeglich, es gibt keine Piloten zum Entfernen");
+			return false;
+		}
+	}
+
+	
+	
+	
+	
+	
+}
