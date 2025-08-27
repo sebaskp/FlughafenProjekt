ef3569186ffbefea02b28c58f82ae7e0d9eb65ea
mikab <mikab@Mika.fritz.box>
2025-08-27 12:38:37 +0200

    neue Klasse Flugzeug

--- Flughafen/src/spe/mch/Flugzeug.java
+++ Flughafen/src/spe/mch/Flugzeug.java
@@ -3,5 +3,57 @@ package spe.mch;
 import java.io.Serializable;
 
 public class Flugzeug implements Serializable{
-
+//	Konstruktoren
+	private String type;
+	private int anzahlPlaetze;
+	private boolean tankVoll;
+	
+	private Flugzeug(String type,int anzahlPlaetze, boolean tankVoll) {
+		this.type=type;
+		this.anzahlPlaetze=anzahlPlaetze;
+		this.tankVoll=false;
+	}
+	private Flugzeug() {
+		
+	}
+	public int getAnzahlPlaetze() {
+		return anzahlPlaetze;
+	}
+	public void setAnzahlPlaetze(int anzahlPlaetze) {
+		this.anzahlPlaetze = anzahlPlaetze;
+	}
+	public boolean isTankVoll() {
+		return tankVoll;
+	}
+	public void setTankVoll(boolean tankVoll) {
+		this.tankVoll = tankVoll;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+	@Override
+	public String toString() {
+		return "Flugzeug [type=" + type + ", anzahlPlaetze=" + anzahlPlaetze + ", tankVoll=" + tankVoll + "]";
+	}
+//	Methoden
+	
+	public void tanken() {
+		if(!tankVoll) {
+			tankVoll=true;
+		}
+	}
+	
+	public void VerbraucheTreibstoff() {
+		if(tankVoll) {
+			tankVoll=false;
+		}
+	}
+	
+	public boolean istTankVoll() {
+		}
+		return System.out.println(tankVoll);
+		
 }
